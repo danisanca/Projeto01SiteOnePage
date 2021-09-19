ed7f9cd2e6c9c1b214e8d2c9b0c38bdc86b28d6b
Daniel Mendonça <dani_sanca@hotmail.com>
2021-09-19 11:31:34 -0300

    :muscle: Implementação de menu mobile e ajuste de menu

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,30 @@ $('.slide-principal-img').slick({
     autoplay:true,
     autoplaySpeed: 10000
   });
-          
+
+
+document.querySelector(".Menu-Mobile").addEventListener("click",()=>
+  
+    document.querySelector(".menu").classList.toggle("show-menu")
+    
+);
+
+function iconeinicio(){
+const windowTop = window.pageYOffset;
+const icone = document.querySelector(".button-home");
+let showbutton = false;
+if(windowTop > 420){
+  document.querySelector(".button-home").classList.add("show")
+}
+else{
+  
+    document.querySelector(".button-home").classList.remove("show")
+  
+}
+console.log(windowTop);
+
+}
+
+window.addEventListener('scroll', () => {
+  iconeinicio();
+})
